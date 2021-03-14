cdc1782ccfc948fc72eb1dc7af77f958d80213a3
nmhummel <natalie.hummel@gmail.com>
2021-03-13 18:01:30 -0600

    commit tree 464faa92395afb5972b05273e0d0c99bcdf32c9d

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,13 @@
 // TODO: Create action creators as defined in tests
+const ADD = "ADD_QUOTE"
+const REMOVE = "REMOVE_QUOTE"
+const UPVOTE = "UPVOTE_QUOTE"
+const DOWNVOTE = "DOWNVOTE_QUOTE"
+
+export const addQuote = (quote) => ({type: ADD, quote: Object.assign({}, quote, { votes: 0 }) })
+
+export const removeQuote = (quoteId) => ({type: REMOVE, quoteId})
+
+export const upvoteQuote = (quoteId) => ({type: UPVOTE, quoteId})
+
+export const downvoteQuote = (quoteId) => ({type: DOWNVOTE, quoteId})
